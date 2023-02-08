bdf424bbd94f84371cf0130d03d55e287d7376da
nitish.d <nitish.d@technovert.com>
2023-02-08 11:35:23 +0530

    ...

---
+++ ParkingLotStimulation.Modal/Slot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotStimulation
+{
+    public class Slot : Ticket
+    {
+        string _slotNumber;
+        Boolean _isBooked;
+        public Slot(string slotPrefix, Boolean isbooked, int index)
+        {
+            _slotNumber = slotPrefix + index;
+            _isBooked = isbooked;
+        }
+
+        public string SlotNumber
+        {
+            get { return _slotNumber; }
+            set { _slotNumber = value; }
+        }
+        public Boolean IsBooked
+        {
+            get { return _isBooked; }
+            set { _isBooked = value; }
+        }
+    }
+}
